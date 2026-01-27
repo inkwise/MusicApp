083b2d3e3ca886b6579064f88d2fd554bc66d47b
inkwise <vacaaa@163.com>
2026-01-27 17:54:12 +0800

    e0.0.016

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,6 +124,5 @@ dependencies {
 	implementation ("androidx.room:room-runtime:2.6.1")
   	kapt ("androidx.room:room-compiler:2.6.1" )
 	implementation ("androidx.room:room-ktx:2.6.1")
-	classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
 	
 }
