1d978845063d291bbb88c75d170022b670b8b9ed
inkwise <vacaaa@163.com>
2026-01-27 17:37:25 +0800

    e0.0.007

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,5 +122,6 @@ dependencies {
 	implementation ("androidx.hilt:hilt-navigation-compose:1.2.0" )
 	implementation ("androidx.room:room-runtime:2.6.1")
   	kapt ("androidx.room:room-compiler:2.6.1" )
-	implementation ("androidx.room:room-ktx:2)
+	implementation ("androidx.room:room-ktx:2.6.1")
+	
 }
