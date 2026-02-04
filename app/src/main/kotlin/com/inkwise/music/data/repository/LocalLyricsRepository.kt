eabc53b5216f785d70b7f6b9420502ce9c1cca9a
inkwise <vacaaa@163.com>
2026-02-04 22:25:40 +0800

    0.0.152

--- app/src/main/kotlin/com/inkwise/music/data/repository/LocalLyricsRepository.kt
+++ app/src/main/kotlin/com/inkwise/music/data/repository/LocalLyricsRepository.kt
@@ -16,59 +16,7 @@ import org.jaudiotagger.audio.AudioFileIO
 import org.jaudiotagger.tag.FieldKey
 import java.io.File
 import android.content.Context
-/*
-class LocalLyricsRepository @Inject constructor(): LyricsRepository {
 
-    private val cache = mutableMapOf<Long, Lyrics>()
-
-    override suspend fun loadLyrics(songId: Long): Lyrics? {
-	    val song = songRepository.getSong(songId) ?: return null
-	
-	    // 1️⃣ 优先 LRC（可同步）
-	    loadLrcFromDisk(song)?.let { return it }
-	
-	    // 2️⃣ 内嵌歌词（不可同步）
-	    loadEmbeddedLyrics(song)?.let { return it }
-	
-	    // 3️⃣ 在线歌词（以后扩展）
-	    // loadFromNetwork(song)
-	
-	    return null
-	}
-
-    override fun observeLyrics(songId: Long): Flow<Lyrics?> = flow {
-        val lyrics = loadLyrics(songId)
-        emit(lyrics)
-    }
-
-    private suspend fun loadFromDisk(songId: Long): Lyrics? {
-        // TODO: 以后实现 lrc / krc 解析
-        return null
-    }
-    private fun loadEmbeddedLyrics(song: Song): Lyrics? {
-	    return try {
-	        val audioFile = AudioFileIO.read(File(song.path))
-	        val tag = audioFile.tag ?: return null
-	
-	        val lyricText = tag.getFirst(FieldKey.LYRICS)
-	        if (lyricText.isBlank()) return null
-	
-	        Lyrics(
-	            lines = lyricText
-	                .lines()
-	                .filter { it.isNotBlank() }
-	                .mapIndexed { index, line ->
-	                    LyricLine(
-	                        timeMs = index * 5_000L, // ❗ 无时间戳，只能伪造
-	                        text = line
-	                    )
-	                }
-	        )
-	    } catch (e: Exception) {
-	        null
-	    }
-	}
-}*/
 class LocalLyricsRepository @Inject constructor(
     private val musicRepository: MusicRepository,
     @ApplicationContext private val context: Context
@@ -77,10 +25,13 @@ class LocalLyricsRepository @Inject constructor(
     private val cache = mutableMapOf<Long, Lyrics>()
 
     override suspend fun loadLyrics(songId: Long): Lyrics? {
+    	toast("loadLyrics()")
+		
         cache[songId]?.let { return it }
 
         val song = musicRepository.getSongById(songId) ?: return null
 		toast("正在读取内嵌歌词")
+		
         // 1️⃣ 内嵌歌词（逐行）
         loadEmbeddedLyrics(song)?.let {
             cache[songId] = it
