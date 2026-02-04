d892d653e1cfbec647e2675a804bfd3bedde1eca
inkwise <vacaaa@163.com>
2026-02-04 21:02:46 +0800

    e0.0.128

---
+++ app/src/main/kotlin/com/inkwise/music/di/LyricsModule.kt
@@ -0,0 +1,20 @@
+package com.inkwise.music.di
+
+import com.inkwise.music.data.repository.LyricsRepository
+import com.inkwise.music.data.repository.LocalLyricsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LyricsModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLyricsRepository(
+        impl: LocalLyricsRepository
+    ): LyricsRepository
+}
