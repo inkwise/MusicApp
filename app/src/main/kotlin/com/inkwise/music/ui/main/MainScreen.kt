4d88df590c7cbcdf27b9a988b41e760a3769985c
inkwise <vacaaa@163.com>
2026-02-08 18:19:20 +0800

    0.0.287

--- app/src/main/kotlin/com/inkwise/music/ui/main/MainScreen.kt
+++ app/src/main/kotlin/com/inkwise/music/ui/main/MainScreen.kt
@@ -1301,7 +1301,7 @@ fun BottomDrawerContent(
                 .statusBarsPadding() // 自动增加顶部 Padding，高度等于状态栏
                 //.padding(horizontal = 16.dp)
                 //.padding(bottom = 16.dp)
-                .padding(20.dp) //,
+                .padding(28.dp) //,
     ) {
         // ---------- 顶部：歌名 / 歌手 ----------
         Column(
