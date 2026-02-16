47ba6e5c6e903a7ae97055ad1357a8cd2c09ca17
inkwise <vacaaa@163.com>
2026-02-16 08:40:10 +0800

    0.0.552

--- app/src/main/kotlin/com/inkwise/music/ui/main/navigationPage/local/LocalSongsScreen.kt
+++ app/src/main/kotlin/com/inkwise/music/ui/main/navigationPage/local/LocalSongsScreen.kt
@@ -32,11 +32,7 @@ fun LocalSongsScreen(
     val playbackState by playerViewModel.playbackState.collectAsState()
     val songs by localViewModel.localSongs.collectAsState()
     val isScanning by localViewModel.isScanning.collectAsState()
-    // PullRefresh 状态
-    val pullState = rememberPullToRefreshState(
-        isRefreshing = isScanning,
-        onRefresh = { localViewModel.scanSongs(context) }
-    )
+    
     // 首次加载
     LaunchedEffect(Unit) {
         // localViewModel.loadLocalSongsFromStore()
