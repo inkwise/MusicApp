2f1cb49a37f3ead5fa5371353edc533e1d694df8
inkwise <vacaaa@163.com>
2026-02-16 08:15:09 +0800

    e0.0.528

--- app/src/main/kotlin/com/inkwise/music/ui/main/navigationPage/local/LocalSongsScreen.kt
+++ app/src/main/kotlin/com/inkwise/music/ui/main/navigationPage/local/LocalSongsScreen.kt
@@ -63,6 +63,7 @@ fun LocalSongsScreen(
                     modifier = Modifier.size(18.dp),
                 )
             }
+            Text(songs.size)
 
             if (songs.isNotEmpty()) {
                 FilledTonalButton(
